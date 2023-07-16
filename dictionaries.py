c7687e0c435d12bccccca2b6a6304cb92af6a6f6
Vansh Kumar Singh <114809624+vanshksingh@users.noreply.github.com>
2023-07-16 15:50:00 +0530

    Create dictionaries.py

---
+++ dictionaries.py
@@ -0,0 +1,10 @@
+capitals ={'usa':'washington dc',
+           'india':'new delhi',
+           'russia':'moscow',
+           'china':'bejing'}
+
+capitals.update({'Germany':'Berlin'})
+#print(capitals['russia'])
+print(capitals.get('russia'))
+print(capitals.values())
+print(capitals.items())
