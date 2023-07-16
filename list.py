4a5b5ef1a3eb52efc3e86629fb384aaabd993956
Vansh Kumar Singh <114809624+vanshksingh@users.noreply.github.com>
2023-07-16 15:31:15 +0530

    Create list.py

---
+++ list.py
@@ -0,0 +1,14 @@
+food = ["pizza", "hamburgers", "noodles"]
+food[0] = "sushi"
+print(food[0])
+
+
+food.append("car")
+food.remove("noodles")
+food.pop()
+food.insert(1, "spaceship")
+food.sort()
+#food.cear()
+
+for x in food:
+    print(x)
